8089a22eaf07e570748332733ef260cb07fd8562
Hrishikesh <hrishikeshgirnekar99@gmail.com>
2025-10-12 16:23:59 +0530

    Initial Project commit

---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,84 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './LoginPopup.css'
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios'
+
+const LoginPopup = ({ setShowLogin }) => {
+
+    const {url, setToken} = useContext(StoreContext);
+
+    const [currState, setCurrState] = useState("Login");
+
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        password:"",
+    })
+
+    const onChangeHandler = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setData(data => ({...data, [name]:value}));
+    }
+
+    const onLogin = async(e) =>{
+        e.preventDefault();
+
+        let newUrl = url;
+
+        if(currState === "Login"){
+            newUrl += "/api/user/login"
+        }else{
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl, data);
+
+        if(response.data.success){
+            setToken(response.data.token);
+            localStorage.setItem("token", response.data.token);
+            setShowLogin(false); // after login, login page will be hidden
+        }else{
+            alert(response.data.message);
+        }
+
+
+    }
+
+    // to check the all input fields are working or not
+    // useEffect(() => {
+    //     console.log(data);
+    // },[data]);
+
+    return (
+        <div className='login-popup'>
+            <form onSubmit={onLogin} action="" className="login-popup-container">
+                <div className="login-popup-title">
+                    <h2>{currState}</h2>
+                    <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+                </div>
+                <div className="login-popup-inputs">
+                    {currState === "Login" ? <></> : <input onChange={onChangeHandler} type="text" name="name" value={data.name} id="" placeholder='Your Name' required />}
+
+                    <input onChange={onChangeHandler} type="email" name="email" value={data.email} id="" placeholder='Your Email' required />
+                    <input onChange={onChangeHandler} type="password" name="password" value={data.password} id="" placeholder='Password' />
+                </div>
+                <button type='submit'>{currState === "Sign Up" ? "Create Account" : "Login"}</button>
+                <div className="login-popup-condition">
+                    <input type="checkbox" required />
+                    <p>By contubuing, i agree to the terms of use & privacy policy.</p>
+                </div>
+                {currState === "Login"
+                    ? <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
+                    : <p>Already have an account <span onClick={() => setCurrState("Login")}>Login here</span></p>
+                }
+
+
+            </form>
+        </div>
+    )
+}
+
+export default LoginPopup
---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,96 @@
+import { createContext, useEffect, useState } from "react";
+// import { food_list } from "../assets/assets";
+import axios from 'axios'
+export const StoreContext = createContext(null);
+
+
+const StoreContextProvider = (props) => {
+
+    const [cartItems, setCartItems] = useState({});
+
+    const [token, setToken] = useState("");
+
+    const [food_list, setFoodList] = useState([]);
+
+    const url = "http://localhost:4000";
+
+    const addToCart = async (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        } else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+
+        if(token){
+            await axios.post(url+"/api/cart/add", {itemId}, {headers:{token}});
+        }
+    }
+
+    const removeFromCart = async (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+
+        if(token){
+            await axios.post(url+"/api/cart/remove", {itemId},{headers:{token}});
+        }
+    }
+
+    // useEffect(() => {
+    //     console.log(cartItems);
+    // },[cartItems])   // To check how many items and quantity of that item is there
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+
+        }
+        return totalAmount;
+    }
+
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list");
+        setFoodList(response.data.data);
+    }
+
+    const loadCartData = async(token) => {
+        const response = await axios.post(url+"/api/cart/get", {}, {headers:{token}});
+        setCartItems(response.data.cartData);
+    }
+
+    useEffect(() => {
+
+        async function loadData() {
+            await fetchFoodList();
+            //On reloading, user was logging out, so this will stay user login even though we refresh or reload the page
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+
+                // On reloading or refreshing, cart data was resetting. like if i added 2 items then by refreshing, data gone!
+                await loadCartData(localStorage.getItem("token"))
+            }
+
+        }
+        loadData();
+    }, [])
+
+    const contextValue = {
+        food_list,
+        cartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount,
+        url,
+        token,
+        setToken,
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+export default StoreContextProvider;
