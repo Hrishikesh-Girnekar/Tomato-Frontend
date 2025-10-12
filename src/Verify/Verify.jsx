514d5303a47c7fb062cf46ca06f67eac2921cc97
Hrishikesh <hrishikeshgirnekar99@gmail.com>
2025-10-12 18:40:27 +0530

    Updated code in Verify component in useEffect hook

--- src/Verify/Verify.jsx
+++ src/Verify/Verify.jsx
@@ -6,29 +6,32 @@ import axios from 'axios'
 
 const Verify = () => {
 
-    const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
 
-    const success = searchParams.get("success");
-    const orderId = searchParams.get("orderId");
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
 
-    const { url } = useContext(StoreContext);
+  const { url } = useContext(StoreContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const verifyPayment = async () => {
-        const response = await axios.post(url+"/api/order/verify", {success, orderId});
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", { success, orderId });
 
-        if (response.data.success) {
-          navigate("/myorders");
-        }else{
-          navigate("/");
-        }
+    if (response.data.success) {
+      navigate("/myorders");
+    } else {
+      navigate("/");
     }
+  }
 
-    useEffect(() => {
-      verifyPayment();
-    })
-    
+  useEffect(() => {
+    const runVerification = async () => {
+      await verifyPayment();
+    };
+    runVerification();
+  }, []);
+
   return (
     <div className='div'>
       <div className="spinner">
