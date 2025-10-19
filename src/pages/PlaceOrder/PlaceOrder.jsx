092ee1ba0b2dfebb7383bc353510e9981f71d2a3
Hrishikesh <hrishikeshgirnekar99@gmail.com>
2025-10-19 17:47:18 +0530

    Fix: Delay redirect to show success toast after placing order

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -8,27 +8,27 @@ import { toast } from 'react-toastify';
 
 
 const PlaceOrder = () => {
-  const {getTotalCartAmount, token, food_list, cartItems, url} = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext);
 
   const navigate = useNavigate();
 
   const [data, setData] = useState({
-    firstName:"",
-    lastName:"",
-    email:"",
-    street:"",
-    city:"",
-    state:"",
-    zipcode:"",
-    country:"",
-    phone:""
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: ""
   })
 
-  const onChangeHandler = (e) =>{
-   
+  const onChangeHandler = (e) => {
+
     const name = e.target.name;
-    const value=e.target.value;
-    setData(data=>({...data, [name]:value}));
+    const value = e.target.value;
+    setData(data => ({ ...data, [name]: value }));
 
   }
 
@@ -37,7 +37,7 @@ const PlaceOrder = () => {
 
     let orderItems = [];
     food_list.map((item) => {
-      if(cartItems[item._id] > 0){
+      if (cartItems[item._id] > 0) {
         let itemInfo = item;
         itemInfo["quantity"] = cartItems[item._id];
         orderItems.push(itemInfo);
@@ -46,35 +46,49 @@ const PlaceOrder = () => {
     // console.log(orderItems); For checking, above logic is working or not
 
     let orderData = {
-      address:data,
-      items:orderItems,
-      amount:getTotalCartAmount()+2,
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
     }
 
-    let response = await axios.post(url+"/api/order/place", orderData, {headers:{token}});
+    let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
 
-    if(response.data.success){
+    // if(response.data.success){
+    //   toast.success(response.data.message);
+    //   const {session_url} = response.data;
+    //   window.location.replace(session_url);
+
+    // }else{
+    //   toast.error(response.data.message);
+    // }
+
+    if (response.data.success) {
       toast.success(response.data.message);
-      const {session_url} = response.data;
-      window.location.replace(session_url);
-      
-    }else{
+
+      const { session_url } = response.data;
+
+      // Wait 1 second to show toast before redirect
+      setTimeout(() => {
+        window.location.replace(session_url);
+      }, 2000);
+    } else {
       toast.error(response.data.message);
     }
+
   }
 
   useEffect(() => {
-    if(!token){
+    if (!token) {
       toast.error("You are not Logged in !")
       navigate('/cart')
-    }else if(getTotalCartAmount() == 0){
+    } else if (getTotalCartAmount() == 0) {
       toast.error("Please add some food to cart first !")
       navigate('/cart')
     }
-  },[token])
+  }, [token])
 
   // Instead of above, you can do it in cart page itself with below code by changing onClick event
-  
+
   // const handleCheckout = () => {
   //   if (!token) {
   //     toast.error("You are not logged in!");
@@ -94,9 +108,9 @@ const PlaceOrder = () => {
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
           <input type="text" name='firstName' onChange={onChangeHandler} value={data.firstName} placeholder='First name' required />
-          <input type="text" name='lastName' onChange={onChangeHandler} value={data.lastName} placeholder='Last name' required/>
+          <input type="text" name='lastName' onChange={onChangeHandler} value={data.lastName} placeholder='Last name' required />
         </div>
-        <input type="email" name='email' onChange={onChangeHandler} value={data.email} placeholder='Email address' required/>
+        <input type="email" name='email' onChange={onChangeHandler} value={data.email} placeholder='Email address' required />
         <input type="text" name='street' onChange={onChangeHandler} value={data.street} placeholder='Street' required />
         <div className="multi-fields">
           <input type="text" name='city' onChange={onChangeHandler} value={data.city} placeholder='City' required />
@@ -124,7 +138,7 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() === 0 ? 0 :getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
           <button type='submit'>PROCEED TO PAYMENT</button>
