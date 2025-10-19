a4db12a739b4851f83b4f4279af7567061c1ca56
Hrishikesh <hrishikeshgirnekar99@gmail.com>
2025-10-19 16:47:20 +0530

    Fixed react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
